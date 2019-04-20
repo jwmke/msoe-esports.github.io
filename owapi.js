68edfb506aaaa0247a49f4080bb81169006e295a
Joseph Weller <43486503+Joseph5062A@users.noreply.github.com>
2019-04-19 20:29:38 -0500

    Added stubs for remaining pages and started work on stats page
    Stats page uses jquery to pull json data from an overwatch api to update skill rating of players live

---
+++ owapi.js
@@ -0,0 +1,23 @@
+var getJSON = function(url) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('get', url, true);
+      xhr.responseType = 'json';
+      xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+          resolve(xhr.response);
+        } else {
+          reject(status);
+        }
+      };
+      xhr.send();
+    });
+  };
+  
+  getJSON('https://ow-api.com/v1/stats/pc/us/xPepsi-1642/profile').then(function(data) {
+      ('Your Json result is:  ' + data.ratingIcon); //you can comment this, i used it to debug
+      result.innerText = data.ratingIcon; //display the result in an HTML element
+  }, function(status) { //error detection....
+    alert('Something went wrong.');
+  });
